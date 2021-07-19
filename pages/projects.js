0e11be8cc008f53f9af6271f655db42e1a9aa986
Maxwell Lang <maxwlang@me.com>
2021-07-19 16:04:19 -0500

    Add placeholder component to Projects page

---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+export default class Projects extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      }
+    }
+  
+    render() {
+      return <h1>Coming soon</h1>
+    }
+}
