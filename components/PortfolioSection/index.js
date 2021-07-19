205dc524c800f65fae5b479a31343c7d03eff958
Maxwell Lang <maxwlang@me.com>
2021-07-19 02:00:16 -0500

    PortfolioSection component

--- components/PortfolioSection/index.js
+++ components/PortfolioSection/index.js
@@ -1,29 +1,52 @@
 import { Component } from 'react';
+import Link from 'next/link';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+import Row from 'react-bootstrap/Row';
+
 import styles from './PortfolioSection.module.scss';
 
 export default class PortfolioSection extends Component {    
     constructor(props) {
         super(props);
         this.state = {};
-        console.log(props, this);
     }
-
-    // componentDidUpdate() {}
-    // handleClick = () => this.setState({ myState: 'setVal' });
 
     render() {
         return (
             <div className={styles.container}>
+                {/* Optional header & subheader */}
                 {this.props.title || this.props.subtitle ?
                     <div className={styles.header}>
                         {this.props.title ?
-                            <h1>{this.props.title}</h1>
+                            <h1>
+                                {this.props.title}
+                                {this.props.subtitle ?
+                                    <small> <span className="d-none d-md-inline-block">&#8212;</span> {this.props.subtitle}</small>
+                                : null}
+                            </h1>
                         : null}
+                    </div>
+                : null}
 
-                        {this.props.subtitle ?
-                            <h3>{this.props.subtitle}</h3>
-                        : null}
+                {/* Inner content */}
+                <div className={styles.innerContent}>
+                    <Row>
                         {this.props.children}
+                    </Row>
+                </div>
+
+                {/* Optional next chevron */}
+                {this.props.scrollNextChevron ?
+                    <div className={styles.chevronContainer}>
+                        <Link href={this.props.scrollNextChevronDestination}>
+                            <FontAwesomeIcon
+                                className={styles.chevron}
+                                icon={faChevronDown}
+                            />
+                        </Link>
                     </div>
                 : null}
             </div>
