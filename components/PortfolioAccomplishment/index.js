aad725bb2507cb9a22773fe380da7fb923e28b48
Maxwell Lang <maxwlang@me.com>
2021-07-19 01:59:24 -0500

    PortfolioAccomplishment component

--- components/PortfolioAccomplishment/index.js
+++ components/PortfolioAccomplishment/index.js
@@ -1,4 +1,8 @@
 import { Component } from 'react';
+
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
 import styles from './PortfolioAccomplishment.module.scss';
 
 export default class PortfolioAccomplishment extends Component {
@@ -13,8 +17,27 @@ export default class PortfolioAccomplishment extends Component {
 
     render() {
         return (
-            <>
-            </>
+            <Col xs={12} sm={6} lg={4}>
+                <Card className={styles.card}>
+                    <Card.Body>
+                        {this.props.title ?
+                            <Card.Title>{this.props.title}</Card.Title>
+                        : null}
+
+                        {this.props.subtitle ? 
+                            <Card.Subtitle className="mb-2 text-muted">{this.props.subtitle}</Card.Subtitle>
+                        : null}
+
+                        <Card.Text>
+                            {this.props.children}
+                        </Card.Text>
+
+                        {this.props.links ? 
+                            this.props.map(linkObj => <Card.Link href={linkObj.href}>{linkObj.title}</Card.Link>)
+                        : null}
+                    </Card.Body>
+                </Card>
+            </Col>
         )
     }
 };
