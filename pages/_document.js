fbe01bb0510aeb8fec4827a71744f080efdeb362
Maxwell Lang <maxwlang@me.com>
2021-07-20 15:32:32 -0500

    Fix broken document

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,33 @@
-import { Head } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
   render() {
     return (
-      <Head>
-        <script src="https://www.googletagmanager.com/gtag/js?id=G-24SMZDF52M" async />
+      <Html>
+        <Head>
+            <script src="https://www.googletagmanager.com/gtag/js?id=G-24SMZDF52M" async />
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-24SMZDF52M', { page_path: window.location.pathname });
-            `,
-          }}
-        />
-      </Head>
-    );
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-24SMZDF52M', { page_path: window.location.pathname });
+                    `,
+                }}
+            />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
