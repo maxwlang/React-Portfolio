f9961d85b19d667d5d87f71e245b9dd908cf158b
Maxwell Lang <maxwlang@gmail.com>
2025-01-26 22:57:51 -0600

    extend tailwind instead of overwrite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,24 +7,24 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    keyframes: {
-      gradient: {
-        '0%, 100%': { backgroundPosition: '0% 50%' },
-        '50%': { backgroundPosition: '100% 50%' },
-      },
-      cursor: {
-        "0%, 100%": { opacity: "1" },
-        "50%": { opacity: "0" },
-      },
-    },
-    animation: {
-      gradient: 'gradient 3s ease infinite',
-      cursor: "cursor 0.85s steps(2, start) infinite",
-    },
     extend: {
       colors: {
         background: '#1e293b',
         foreground: '#e2e8f0'
+      },
+      animation: {
+          gradient: 'gradient 3s ease infinite',
+          cursor: "cursor 0.85s steps(2, start) infinite",
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+        cursor: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0" },
+        },
       },
     },
   },
